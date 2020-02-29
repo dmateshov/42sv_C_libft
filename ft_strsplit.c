19f228f2806ed277c834e160db500c158c2f5a0a
Dauren Mateshov <dmatesho@e1z3r9p5.42.us.org>
2020-02-28 22:06:03 -0800

    split

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,113 +6,28 @@
 /*   By: dmatesho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 14:45:16 by dmatesho          #+#    #+#             */
-/*   Updated: 2020/02/28 21:43:41 by dmatesho         ###   ########.fr       */
+/*   Updated: 2020/02/28 22:05:29 by dmatesho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-static int	word_count(const char *s, char c)
-{
-	int words;
-
-	words = 0;
-	while (*s == c)
-		s++;
-	while (*s)
-	{
-		if (*s == c)
-		{
-			while (*s == c)
-				s++;
-			words++;
-		}
-		s++;
-	}
-	return (words);
-}
-/*
-{
-	int			i;
-
-	i = 0;
-	if (*s && *s != c)
-	{
-		s++;
-		i++;
-	}
-	while (*s)
-	{
-		while (*s == c)
-		{
-			s++;
-			if (*s != c && *s)
-				i++;
-		}
-		s++;
-	}
-	return (i);
-}
-*/
-static int		length(const char *s, char c)
-{
-	int i;
-
-	i = 0;
-	while (*s != c && *s)
-	{
-		i++;
-		s++;
-	}
-	return (i);
-}
-
-char			**ft_strsplit(char const *s, char c)
-{
-	char		**arr;
-	size_t		i;
-	int			j;
-
-	j = 0;
-	if (!(arr = (char**)malloc(sizeof(char*) * word_count(s, c) + 1)))
-		return (NULL);
-	while (*s)
-	{
-		while (*s == c && *s)
-			s++;
-		i = 0;
-		if (*s != c && *s)
-		{
-			if (!(arr[j] = (char*)malloc(length(s, c) + 1)))
-				return (NULL);
-			while (*s && *s != c)
-				arr[j][i++] = (char)*s++;
-			arr[j][i] = '\0';
-			j++;
-		}
-	}
-	arr[j] = NULL;
-	return (arr);
-}
-
-/*
 static int	ft_wdct(char const *s, char c)
 {
 	int words;
 
 	words = 0;
-	while (*s == c)
-		s++;
 	while (*s)
 	{
-		if (*s == c)
+		while (*s == c)
+			s++;
+		if (*s && *s != c)
 		{
-			while (*s == c)
+			while (*s && *s != c)
 				s++;
 			words++;
 		}
-		s++;
 	}
 	return (words);
 }
@@ -157,4 +72,3 @@ char		**ft_strsplit(char const *s, char c)
 	ptr[i] = NULL;
 	return (ptr);
 }
-*/
